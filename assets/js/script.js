436c9b348dd11ff931ccbe482db32418d150cdb0
Declan <decofeco29@gmail.com>
2017-10-03 16:17:08 +0000

    Home page finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,26 @@
-$(document).ready(function()
+$(function(){
+  $('#nav').data('size','big');
+});
+
+$(window).scroll(function(){
+  if($(document).scrollTop() > 0)
 {
-    var navbar = $('nav');
-    if (window.width() <= '768px') 
+    if($('#nav').data('size') == 'big')
     {
-        navbar.removeClass('navbar-fixed-top');
+        $('#nav').data('size','small');
+        $('#nav').stop().animate({
+            height:'40px'
+        },2000);
     }
+}
+else
+  {
+    if($('#nav').data('size') == 'small')
+      {
+        $('#nav').data('size','big');
+        $('#nav').stop().animate({
+            height:'180px'
+        },2000);
+      }  
+  }
 });
