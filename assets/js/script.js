1ff37a88c047cd4f458b471a0ba45f39b5c69357
Declan <decofeco29@gmail.com>
2017-10-04 16:25:14 +0000

    Timeline implemented and model set up for the form submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,7 @@
-$(function(){
-  $('#nav').data('size','big');
+$( "form" ).submit(function( event ) {
+  event.preventDefault();
 });
 
-$(window).scroll(function(){
-  if($(document).scrollTop() > 0)
-{
-    if($('#nav').data('size') == 'big')
-    {
-        $('#nav').data('size','small');
-        $('#nav').stop().animate({
-            height:'40px'
-        },2000);
-    }
-}
-else
-  {
-    if($('#nav').data('size') == 'small')
-      {
-        $('#nav').data('size','big');
-        $('#nav').stop().animate({
-            height:'180px'
-        },2000);
-      }  
-  }
-});
+function handleClick(){
+  $('#myModal').modal('show')
+};
