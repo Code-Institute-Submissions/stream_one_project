584c1305e78ac524d1495d925de99b171ca62d8a
Declan <decofeco29@gmail.com>
2017-10-05 18:03:59 +0000

    footer modification part1

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,12 @@ function handleClick(){
   $('#myModal').modal('show')
   $('form').trigger("reset")
 };
+
+$(document).ready(function(){
+    if ($(window).width() >= 768) 
+    {
+	    $(".footer-static-bottom").hover(function () {
+    $(".slide").slideToggle("fast");
+  });
+    }
+});
