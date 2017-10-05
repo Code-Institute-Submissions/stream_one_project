71eea4e6a8fb3a1dd70e49892d0d58a6a00ad770
Declan <decofeco29@gmail.com>
2017-10-05 13:24:58 +0000

    Modal working and retrieving data from the contact form. Needs styling

---
+++ testing/spec/siteSpec.js
@@ -0,0 +1,21 @@
+// describe('handleClick', function() { 
+    
+    
+//     it('should open the modal', function() {
+//         spyOn($modal, 'open');
+//         handleClick();
+//         expect($modal.open).toHaveBeenCalled();
+//     });
+// });
+
+// var value = 'some value';
+// var differentValue = 'different value';
+
+// beforeEach(function () {
+//   setFixtures($('<input id="name" type="text" />').val(value));
+// });
+
+// it("should pass if value matches expectation", function () {
+//   expect($('#name')).toHaveValue(value);
+//   //expect($('#name').get(0)).toHaveValue(value);
+// });
